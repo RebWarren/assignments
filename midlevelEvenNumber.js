34676385a217886265a881ea9dc0babcaf92ad7d
Becky Warren <rebjwarren@gmail.com>
2024-02-14 00:17:55 -0500

    Create midlevelEvenNumber.js

---
+++ midlevelEvenNumber.js
@@ -0,0 +1,7 @@
+function findEvenNumbers(array){
+    for (var i = 0; i < findEvenNumbers.length; i++) 
+        if(i % 2 === 0){ 
+    console.log(findEvenNumbers([1,2,3,4,5,6,7,8,9,10]));
+    console.log(findEvenNumbers([11, 22, 33, 44, 55, 66, 77, 88, 99, 100]));
+    }
+}
